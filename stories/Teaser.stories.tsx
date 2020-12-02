d009a0e9c18f61444ffb94265852299cc175b97c
Lukas Wochner <lukas.wochner@blateral.com>
2020-12-02 10:56:52 +0100

    adding more teaser stories

--- stories/Teaser.stories.tsx
+++ stories/Teaser.stories.tsx
@@ -232,97 +232,59 @@ storiesOf('Sections / Teaser', module)
                 </Button.View>
             )}
         />
+    ))
+    .add('with 4:3 image', () => (
+        <Teaser
+            bgMode="splitted"
+            superTitle={'Lorem Ipsum Dolor'}
+            title={'Lorem ipsum dolor sit amet, consetetur sadipscing elitr'}
+            image={{
+                small: 'https://unsplash.it/587/350',
+                medium: 'https://unsplash.it/852/508',
+                large: 'https://unsplash.it/1200/600',
+                xlarge: 'https://unsplash.it/1400/1050',
+                description:
+                    'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ',
+            }}
+            intro="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. "
+            text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+            primaryAction={(isInverted) => (
+                <Button.View isInverted={isInverted}>
+                    <Button.Label>Primary</Button.Label>
+                </Button.View>
+            )}
+            secondaryAction={(isInverted) => (
+                <Button.View type="ghost" isInverted={isInverted}>
+                    <Button.Label>Secondary</Button.Label>
+                </Button.View>
+            )}
+        />
+    ))
+    .add('with 3:4 image', () => (
+        <Teaser
+            bgMode="splitted"
+            superTitle={'Lorem Ipsum Dolor'}
+            title={'Lorem ipsum dolor sit amet, consetetur sadipscing elitr'}
+            image={{
+                small: 'https://unsplash.it/587/350',
+                medium: 'https://unsplash.it/852/508',
+                large: 'https://unsplash.it/1200/1600',
+                xlarge: 'https://unsplash.it/1400/1866',
+                description:
+                    'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ',
+            }}
+            intro="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. "
+            text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.<br /><ul><li>Lorem Ipsum</li><li>Lorem Ipsum</li><li>Lorem Ipsum</li></ul>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua."
+            subText="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua."
+            primaryAction={(isInverted) => (
+                <Button.View isInverted={isInverted}>
+                    <Button.Label>Primary</Button.Label>
+                </Button.View>
+            )}
+            secondaryAction={(isInverted) => (
+                <Button.View type="ghost" isInverted={isInverted}>
+                    <Button.Label>Secondary</Button.Label>
+                </Button.View>
+            )}
+        />
     ));
-// .add('with background', () => (
-//     <Teaser
-//         hasBack
-//         superTitle={'Lorem Ipsum Dolor'}
-//         title={'Lorem ipsum dolor sit amet, consetetur sadipscing elitr'}
-//         image={{
-//             small: 'https://unsplash.it/587/350',
-//             medium: 'https://unsplash.it/852/508',
-//             large: 'https://unsplash.it/1200/911',
-//             xlarge: 'https://unsplash.it/1400/1063',
-//             description:
-//                 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ',
-//         }}
-//         intro="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. "
-//         text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.<br /><ul><li>Lorem Ipsum</li><li>Lorem Ipsum</li><li>Lorem Ipsum</li></ul>"
-//         action={
-//             <>
-//                 <Button.View>
-//                     <Button.Label>Zur ärztlichen Versorgung</Button.Label>
-//                     <Button.Icon>
-//                         <ArrowRight />
-//                     </Button.Icon>
-//                 </Button.View>
-//                 <Button.View type="ghost">
-//                     <Button.Label>mehr lesen</Button.Label>
-//                 </Button.View>
-//             </>
-//         }
-//     />
-// ))
-// .add('with decorator', () => (
-//     <Teaser
-//         hasBack
-//         hasDecorator
-//         superTitle={'Lorem Ipsum Dolor'}
-//         title={'Lorem ipsum dolor sit amet, consetetur sadipscing elitr'}
-//         image={{
-//             small: 'https://unsplash.it/587/350',
-//             medium: 'https://unsplash.it/852/508',
-//             large: 'https://unsplash.it/1200/911',
-//             xlarge: 'https://unsplash.it/1400/1063',
-//             description:
-//                 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ',
-//         }}
-//         intro="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. "
-//         text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.<br /><ul><li>Lorem Ipsum</li><li>Lorem Ipsum</li><li>Lorem Ipsum</li></ul>"
-//         action={
-//             <>
-//                 <Button.View>
-//                     <Button.Label>Zur ärztlichen Versorgung</Button.Label>
-//                     <Button.Icon>
-//                         <ArrowRight />
-//                     </Button.Icon>
-//                 </Button.View>
-//                 <Button.View type="ghost">
-//                     <Button.Label>mehr lesen</Button.Label>
-//                 </Button.View>
-//             </>
-//         }
-//     />
-// ))
-// .add('mirrored', () => (
-//     <Teaser
-//         hasBack
-//         hasDecorator
-//         isMirrored
-//         superTitle={'Lorem Ipsum Dolor'}
-//         title={'Lorem ipsum dolor sit amet, consetetur sadipscing elitr'}
-//         image={{
-//             small: 'https://unsplash.it/587/350',
-//             medium: 'https://unsplash.it/852/508',
-//             large: 'https://unsplash.it/1200/911',
-//             xlarge: 'https://unsplash.it/1400/1063',
-//             description:
-//                 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ',
-//         }}
-//         intro="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. "
-//         text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.<br /><ul><li>Lorem Ipsum</li><li>Lorem Ipsum</li><li>Lorem Ipsum</li></ul>"
-//         action={
-//             <>
-//                 <Button.View>
-//                     <Button.Label>Zur ärztlichen Versorgung</Button.Label>
-//                     <Button.Icon>
-//                         <ArrowRight />
-//                     </Button.Icon>
-//                 </Button.View>
-//                 <Button.View type="ghost">
-//                     <Button.Label>mehr lesen</Button.Label>
-//                 </Button.View>
-//             </>
-//         }
-//     />
-// ));
