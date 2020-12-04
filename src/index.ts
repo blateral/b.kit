1173ec98799b53ca6c2d276f311905865051d3a6
Lukas Wochner <lukas.wochner@blateral.com>
2020-12-04 17:53:50 +0100

    updating gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
 import GallerySection from './components/sections/Gallery';
 
+// styled.d.ts
+import 'styled-components';
+
+interface IPalette {
+    main: string;
+    contrastText: string;
+}
+
+declare module 'styled-components' {
+    export interface DefaultTheme {
+        borderRadius: string;
+        palette: {
+            common: {
+                black: string;
+                white: string;
+            };
+            primary: IPalette;
+            secondary: IPalette;
+        };
+    }
+}
+
 export const Gallery = GallerySection;
