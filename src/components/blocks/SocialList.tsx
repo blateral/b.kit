16d26ded2e750909c02c5390c5dcd6393cad6871
Lukas Wochner <lukas.wochner@blateral.com>
2020-12-14 12:39:30 +0100

    adding block SocialList

---
+++ src/components/blocks/SocialList.tsx
@@ -0,0 +1,71 @@
+import React, { FC } from 'react';
+import styled, { ThemeContext } from 'styled-components';
+
+import { getColors as color, spacings } from '../../utils/styles';
+import Link from '../typography/Link';
+
+const View = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding: ${spacings.nudge * 2}px 0;
+
+    & > * + * {
+        margin-left: ${spacings.nudge * 3}px;
+    }
+`;
+
+const IconItemView = styled.div`
+    transition: all ease-in-out 0.2s;
+
+    &:hover {
+        transform: scale(1.1);
+    }
+
+    &:focus {
+        text-decoration: underline;
+        transform: scale(1.012);
+    }
+
+    &:active {
+        transform: scale(0.95);
+    }
+`;
+
+const StyledLink = styled(Link)<{ textColor?: string }>`
+    display: block;
+    padding: ${spacings.nudge * 2}px ${spacings.nudge * 1.5}px;
+    margin: -${spacings.nudge * 2}px -${spacings.nudge * 1.5}px;
+    color: ${({ theme, textColor }) => textColor || color(theme).black};
+`;
+
+const SocialList: FC<{
+    isInverted?: boolean;
+    items?: { href: string; icon: React.ReactNode }[];
+    className?: string;
+}> = ({ isInverted = false, items, className }) => {
+    const theme = React.useContext(ThemeContext);
+
+    return (
+        <View className={className}>
+            {items &&
+                items.map((item, i) => (
+                    <IconItemView key={i}>
+                        <StyledLink
+                            isExternal
+                            href={item.href}
+                            textColor={
+                                isInverted
+                                    ? color(theme).white
+                                    : color(theme).black
+                            }
+                        >
+                            {item.icon}
+                        </StyledLink>
+                    </IconItemView>
+                ))}
+        </View>
+    );
+};
+
+export default SocialList;
