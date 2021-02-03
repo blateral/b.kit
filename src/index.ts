cd21bd721c85b7e6a14b2f0d0824b36526a701d4
Lukas Wochner <lukas.wochner@blateral.com>
2021-02-03 17:31:32 +0100

    adding instagram icon to index.ts export

--- src/index.ts
+++ src/index.ts
@@ -58,4 +58,5 @@ export { default as FacebookIcon } from 'components/base/icons/socials/Facebook'
 export { default as LinkedInIcon } from 'components/base/icons/socials/LinkedIn';
 export { default as TwitterIcon } from 'components/base/icons/socials/Twitter';
 export { default as XingIcon } from 'components/base/icons/socials/Xing';
+export { default as InstagramIcon } from 'components/base/icons/socials/Instagram';
 export { default as YoutubeIcon } from 'components/base/icons/socials/Youtube';
