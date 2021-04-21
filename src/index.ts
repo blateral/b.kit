3de0007095e4559bf04fcae7aa4f0d21cd72ad8e
Lukas Wochner <lukas.wochner@blateral.com>
2021-04-21 17:07:17 +0200

    adding Navigation  and some utils to export

--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,20 @@ export {
     mq,
 } from 'utils/styles';
 
+/***** Hooks *****/
+export { useEqualSheetHeight } from 'utils/useEqualSheetHeight';
+export {
+    setCookie,
+    deleteCookie,
+    getCookie,
+} from 'utils/cookie-consent/cookie';
+export { default as useIE } from 'utils/useIE';
+export { useItemHeight } from 'utils/useItemHeight';
+export { getMediaQuery, isBrowser, useMediaQuery } from 'utils/useMediaQuery';
+export { useScroll } from 'utils/useScroll';
+export { usePoster, canUseWebP } from 'utils/usePoster';
+export { default as useInterval } from 'utils/useInterval';
+
 /***** Component exports *****/
 export * from 'utils/LibThemeProvider';
 
@@ -63,6 +77,7 @@ export {
     CookieText,
     CookieActions,
 } from 'components/blocks/CookieConsent';
+export { default as Navigation } from 'components/sections/navigation/Navigation';
 
 /***** Icons *****/
 export { default as ArrowRightIcon } from 'components/base/icons/ArrowRight';
