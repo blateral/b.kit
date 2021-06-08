774c71295dedf367d274dc8b2a3288ef93475428
Lukas Wochner <lukas.wochner@blateral.com>
2021-06-08 17:07:18 +0200

    fixing text background cutoff

--- src/utils/styles.ts
+++ src/utils/styles.ts
@@ -56,6 +56,7 @@ export const styleTextColor = (fallbackColor?: string, gradient?: string) => {
             color: ${fallbackColor};
 
             @supports (-webkit-text-stroke: thin) {
+                padding-top: 1px;
                 background: ${gradient};
                 background-clip: text;
                 -webkit-background-clip: text;
