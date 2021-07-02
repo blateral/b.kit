fb27ca0fdfb291a0fe8f63008648ff794c1a6556
Falco Böhnke <falco.boehnke@blateral.com>
2021-07-02 10:44:04 +0200

    Updating exports

--- src/index.ts
+++ src/index.ts
@@ -68,6 +68,19 @@ export { default as PromotionCarousel } from 'components/sections/carousel/Promo
 export { default as VideoCarousel } from 'components/sections/carousel/VideoCarousel';
 export { default as Footer } from 'components/sections/Footer';
 export { default as Header } from 'components/sections/header/Header';
+export { default as NewsAuthorCard } from 'components/sections/news/NewsAuthorCard';
+export { default as NewsFooter } from 'components/sections/news/NewsFooter';
+export { default as NewsImages } from 'components/sections/news/NewsImages';
+export { default as NewsIntro } from 'components/sections/news/NewsIntro';
+export { default as NewsList } from 'components/sections/news/NewsList';
+export { default as NewsTable } from 'components/sections/news/NewsTable';
+export { default as NewsText } from 'components/sections/news/NewsText';
+
+
+
+
+
+
 
 /***** Buttons *****/
 export { default as Button } from 'components/buttons/Button';
