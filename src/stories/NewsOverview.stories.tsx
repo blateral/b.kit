32b0b4d9eb227edc16353c7da30d5f6f50c2ece7
Maja Merk <maja.merk@blateral.com>
2021-07-05 10:54:15 +0200

    Adding tags to inverted story

--- src/stories/NewsOverview.stories.tsx
+++ src/stories/NewsOverview.stories.tsx
@@ -172,6 +172,24 @@ export const IsInverted: Story = () => (
             text:
                 'Mitten im historischen Altstadtkern von Überlingen liegt das Haus St. Franziskus. Das prachtvolle Gebäude, ursprünglich als Kloster von Franziskanermönchen errichtet, kann auf eine rund 750-jährige Geschichte zurückblicken.',
         }}
+        tags={[
+            'Tag 1',
+            'Tag 2',
+            'Tag 3',
+            'Tag 4',
+            'Tag 5',
+            'Tag 6',
+            'Tag 7',
+            'Tag 8',
+            'Tag 9',
+            'Tag 10',
+            'Tag 11',
+            'Tag 12',
+            'Tag 13',
+            'Tag 14',
+            'Tag 15',
+            'Tag 16',
+        ]}
         news={[
             {
                 tag: 'Secondary Tag',
