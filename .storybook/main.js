9bdb4f76a31a8e1e4ef7aa61e44dd19af2155214
Lukas Wochner <lukas.wochner@blateral.com>
2021-09-07 13:42:52 +0200

    setting storybook controls default to disabled

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,7 +4,6 @@ module.exports = {
         {
             name: '@storybook/addon-essentials',
             options: {
-                controls: false,
                 actions: false,
             },
         },
