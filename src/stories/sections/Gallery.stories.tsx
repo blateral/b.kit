e0f74e0920885230f50d98b7112544bb1ef35451
Lukas Wochner <lukas.wochner@blateral.com>
2021-10-13 09:48:48 +0200

    adding splitted background to gallery

--- src/stories/sections/Gallery.stories.tsx
+++ src/stories/sections/Gallery.stories.tsx
@@ -256,6 +256,32 @@ export const WithBackground: Story = () => (
     />
 );
 
+export const SplittedBackground: Story = () => (
+    <Gallery
+        bgMode="splitted"
+        images={[
+            {
+                small: 'https://unsplash.it/500/246?image=400',
+                medium: 'https://unsplash.it/640/315?image=400',
+                large: 'https://unsplash.it/1024/504?image=400',
+                xlarge: 'https://unsplash.it/1440/710?image=400',
+            },
+            {
+                small: 'https://unsplash.it/630/630?image=401',
+                medium: 'https://unsplash.it/315/315?image=401',
+                large: 'https://unsplash.it/507/507?image=401',
+                xlarge: 'https://unsplash.it/710/710?image=401',
+            },
+            {
+                small: 'https://unsplash.it/630/630?image=402',
+                medium: 'https://unsplash.it/315/315?image=402',
+                large: 'https://unsplash.it/507/507?image=402',
+                xlarge: 'https://unsplash.it/710/710?image=402',
+            },
+        ]}
+    />
+);
+
 export const Inverted: Story = () => (
     <Gallery
         bgMode="inverted"
