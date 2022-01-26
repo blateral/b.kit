20e7c79c348a2ae2135c6375e3069ef95294f207
Lukas Wochner <lukas.wochner@blateral.com>
2022-01-26 10:21:15 +0100

    fixing equal sheet height calculation to trigger after fonts loaded

--- src/stories/sections/PriceTable.stories.tsx
+++ src/stories/sections/PriceTable.stories.tsx
@@ -33,7 +33,18 @@ export const Default: Story = () => (
                 superTitle: 'Pro Tarif',
                 title: '<h2>100,-</h2>',
                 text: `
-                <p>EUR im Monat oder 2 Monate sparen für einmalig 1999,- EUR im Jahr</p>`,
+                    <p><strong>Für Vereine und gemeinnützige Organisationen</strong></p>
+                                <ul>
+                    <li>ink. 19% MwSt.</li>
+                    <li>bei jährlicher Zahlung (oder 119,- € mtl.)</li>
+                    <li>Unbegrenzt Beiträge</li>
+                    <li>Unbegrenzt Redakteure</li>
+                    <li>Unbegrenzt Speicherplatz</li>
+                    <li>Eigene Domain nutzbar</li>
+                    <li>Auffindbar&nbsp;in Suchmaschinen</li>
+                    <li>Optionales Modul Kleinanzeigen</li>
+                    <li>Optionales Modul Werbepartner</li>
+                </ul>`,
                 action: button,
             },
             {
--- src/utils/useEqualSheetHeight.ts
+++ src/utils/useEqualSheetHeight.ts
@@ -1,4 +1,10 @@
-import { useEffect, useState, MutableRefObject, createRef } from 'react';
+import {
+    useEffect,
+    useState,
+    MutableRefObject,
+    createRef,
+    useCallback,
+} from 'react';
 import { useMediaQuery } from './useMediaQuery';
 
 interface ItemsPerRow {
@@ -38,104 +44,117 @@ export const useEqualSheetHeight = (props: {
         MutableRefObject<HTMLDivElement>[]
     >([]);
 
-    const makeRows = (
-        result: Array<number>[],
-        _: number,
-        index: number,
-        arr: number[]
-    ) => {
-        // make new row after 3 items
-        if (index % itemsPerRow === 0)
-            result.push(arr.slice(index, index + itemsPerRow) as Array<number>);
-        return result;
-    };
+    const setHeights = useCallback(
+        (
+            heights: (number | null)[],
+            selector: string,
+            refs: MutableRefObject<HTMLDivElement>[]
+        ) => {
+            const makeRows = (
+                result: Array<number>[],
+                _: number,
+                index: number,
+                arr: number[]
+            ) => {
+                // make new row after 3 items
+                if (index % itemsPerRow === 0)
+                    result.push(
+                        arr.slice(index, index + itemsPerRow) as Array<number>
+                    );
+                return result;
+            };
 
-    const setHeights = (
-        heights: (number | null)[],
-        selector: string,
-        refs: MutableRefObject<HTMLDivElement>[]
-    ) => {
-        const x = heights.reduce(makeRows, []);
+            const x = heights.reduce(makeRows, []);
 
-        x.forEach((rowHeights, y) => {
-            const highest = rowHeights.slice().sort((vA, vB) => {
-                if (vA && vB) {
-                    if (vA > vB) return -1;
-                    return 1;
-                } else return 0;
-            })[0];
+            x.forEach((rowHeights, y) => {
+                const highest = rowHeights.slice().sort((vA, vB) => {
+                    if (vA && vB) {
+                        if (vA > vB) return -1;
+                        return 1;
+                    } else return 0;
+                })[0];
 
-            // set height of all items in row to the highest value
-            if (rowHeights.length > 1 && highest) {
-                for (let x = 0; x < rowHeights.length; x++) {
-                    const index = x + y * itemsPerRow;
-                    const el = refs[index].current.querySelector(selector);
+                // set height of all items in row to the highest value
+                if (rowHeights.length > 1 && highest) {
+                    for (let x = 0; x < rowHeights.length; x++) {
+                        const index = x + y * itemsPerRow;
+                        const el = refs[index].current.querySelector(selector);
 
-                    if (el) {
-                        el.setAttribute('style', `height: ${highest}px`);
+                        if (el) {
+                            el.setAttribute('style', `height: ${highest}px`);
+                        }
                     }
                 }
-            }
-        });
-    };
-
-    const resize = (refs: MutableRefObject<HTMLDivElement>[]) => () => {
-        const identifierSizes = new Map<string, Array<number | null>>();
-
-        // create identifier map
-        for (let i = 0; i < props.identifiers.length; i++) {
-            identifierSizes.set(props.identifiers[i], []);
-        }
-
-        // set height values
-        refs.filter((ref) => ref.current).forEach(({ current }) => {
-            // get height for each element
-            props.identifiers.forEach((id) => {
-                const element = current.querySelector(id);
-                element?.removeAttribute('style');
-
-                const prevHeights = identifierSizes.get(id);
-                if (prevHeights)
-                    identifierSizes.set(id, [
-                        ...prevHeights,
-                        element ? element.scrollHeight : null,
-                    ]);
             });
-        });
-
-        // set heights for each identifier
-        identifierSizes.forEach((value, key) => {
-            setHeights(value, key, refs);
-        });
-    };
+        },
+        [itemsPerRow]
+    );
 
     useEffect(() => {
+        const resize = (refs: MutableRefObject<HTMLDivElement>[]) => () => {
+            const identifierSizes = new Map<string, Array<number | null>>();
+
+            // create identifier map
+            for (let i = 0; i < props.identifiers.length; i++) {
+                identifierSizes.set(props.identifiers[i], []);
+            }
+
+            // set height values
+            refs.filter((ref) => ref.current).forEach(({ current }) => {
+                // get height for each element
+                props.identifiers.forEach((id) => {
+                    const element = current.querySelector(id);
+
+                    if (element) {
+                        element.removeAttribute('style');
+
+                        const newHeight = element.scrollHeight;
+
+                        const prevHeights = identifierSizes.get(id);
+                        if (prevHeights) {
+                            identifierSizes.set(id, [
+                                ...prevHeights,
+                                newHeight,
+                            ]);
+                        }
+                    }
+                });
+            });
+
+            // set heights for each identifier
+            identifierSizes.forEach((value, key) => {
+                setHeights(value, key, refs);
+            });
+        };
+
         const resizeHandler = resize(sheetRefs);
         resizeHandler();
 
         window.addEventListener('resize', resizeHandler);
 
+        (document as any)?.fonts?.ready?.then(() => {
+            resizeHandler();
+        });
+
         return () => {
             window.removeEventListener('resize', resizeHandler);
         };
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [sheetRefs, currentMq]);
+    }, [sheetRefs, currentMq, props.identifiers, setHeights]);
 
     const triggerCalculation = () => {
-        setSheetRefs(() =>
+        setSheetRefs((prev) =>
             Array(props.listLength)
                 .fill(null)
-                .map((_, i) => sheetRefs[i] || createRef())
+                .map((_, i) => prev[i] || createRef())
         );
     };
 
     useEffect(() => {
-        setSheetRefs(() =>
+        setSheetRefs((prev) =>
             Array(props.listLength)
                 .fill(null)
-                .map((_, i) => sheetRefs[i] || createRef())
+                .map((_, i) => prev[i] || createRef())
         );
-        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [props.listLength]);
 
     return {
