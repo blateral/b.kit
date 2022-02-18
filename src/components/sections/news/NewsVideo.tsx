3cbc27a8d43db18833baebc6f2d67a4baaa96b76
Maja Merk <maja.merk@blateral.com>
2022-02-18 16:07:48 +0100

    Adjusting spacings

--- src/components/sections/news/NewsVideo.tsx
+++ src/components/sections/news/NewsVideo.tsx
@@ -3,14 +3,14 @@ import styled, { ThemeContext } from 'styled-components';
 
 import { ImageProps } from 'components/blocks/Image';
 import Section, { mapToBgMode } from 'components/base/Section';
-import { getColors as color, spacings, withRange } from 'utils/styles';
+import { getColors as color, spacings } from 'utils/styles';
 import { withLibTheme } from 'utils/LibThemeProvider';
 import Wrapper from 'components/base/Wrapper';
 import VideoCard from 'components/blocks/VideoCard';
 import Actions from 'components/blocks/Actions';
 
 const StyledActions = styled(Actions)`
-    ${withRange([spacings.spacer, spacings.spacer * 2], 'margin-top')};
+    margin-top: ${spacings.spacer}px;
 `;
 
 const NewsVideo: React.FC<{
