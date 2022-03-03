4fca531dbd25af58bbb01518dcf8f91a5953542d
Lukas Wochner <lukas.wochner@blateral.com>
2022-03-03 17:33:58 +0100

    adding header bar to new NavBar

--- src/components/sections/navigation/remastered/NavBar.tsx
+++ src/components/sections/navigation/remastered/NavBar.tsx
@@ -1,22 +1,46 @@
 import React, { FC } from 'react';
 import styled from 'styled-components';
-import { spacings } from 'utils/styles';
+import { spacings, mq } from 'utils/styles';
 import Copy from 'components/typography/Copy';
 
 const View = styled.div`
     margin: 0 auto;
     max-width: ${spacings.wrapperLarge}px;
-    background: cyan;
+`;
+
+const Header = styled.div`
+    background: #7749f8;
+`;
+
+const Main = styled.div`
+    background: lightgrey;
+`;
+
+const ContentTop = styled.div<{ size?: NavBarSize }>`
+    display: flex;
+    align-items: center;
+    height: 30px;
+    max-width: ${spacings.wrapper}px;
+    padding: ${spacings.nudge}px ${spacings.nudge * 2}px;
+    margin: 0 auto;
+
+    @media ${mq.semilarge} {
+        height: 40px;
+    }
 `;
 
 const Content = styled.div<{ size?: NavBarSize }>`
     display: flex;
-    height: ${({ size }) => (size === 'large' ? '120px' : '90px')};
+    height: ${({ size }) => (size === 'large' ? '100px' : '80px')};
     max-width: ${spacings.wrapper}px;
     padding: ${spacings.nudge * 2}px;
     margin: 0 auto;
 
     transition: height 0.2s ease-in-out;
+
+    @media ${mq.semilarge} {
+        height: ${({ size }) => (size === 'large' ? '120px' : '90px')};
+    }
 `;
 
 const Column = styled(Copy)`
@@ -62,13 +86,25 @@ const NavBar: FC<{
 }> = ({ size = 'large', className }) => {
     return (
         <View className={className}>
-            <Content size={size}>
-                <LeftCol>Column Left</LeftCol>
-                <CenterCol>
-                    <Logo src="https://via.placeholder.com/320x80" />
-                </CenterCol>
-                <RightCol>Column Right</RightCol>
-            </Content>
+            <Header>
+                <ContentTop size={size}>
+                    <LeftCol size="small" isInverted>
+                        Left
+                    </LeftCol>
+                    <RightCol size="small" isInverted>
+                        Right
+                    </RightCol>
+                </ContentTop>
+            </Header>
+            <Main>
+                <Content size={size}>
+                    <LeftCol>Column Left</LeftCol>
+                    <CenterCol>
+                        <Logo src="https://via.placeholder.com/320x80" />
+                    </CenterCol>
+                    <RightCol>Column Right</RightCol>
+                </Content>
+            </Main>
         </View>
     );
 };
