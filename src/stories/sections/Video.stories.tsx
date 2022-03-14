4befe2067f6574ae7533735f5f45e0e62213ff33
Maja Merk <maja.merk@blateral.com>
2022-03-14 15:08:12 +0100

    Adding new play button and splitted background to video

--- src/stories/sections/Video.stories.tsx
+++ src/stories/sections/Video.stories.tsx
@@ -40,6 +40,20 @@ export const WithBackground: Story = () => (
     />
 );
 
+export const WithSplittedBackground: Story = () => (
+    <Video
+        bgMode="splitted"
+        bgImage={{
+            small: 'http://unsplash.it/640/480',
+            medium: 'http://unsplash.it/1024/576',
+            large: 'http://unsplash.it/1440/810',
+            xlarge: 'http://unsplash.it/1680/810',
+            alt: 'Placeholder Image',
+        }}
+        embedId="pVE92TNDwUk"
+    />
+);
+
 export const Inverted: Story = () => (
     <Video
         bgMode="inverted"
