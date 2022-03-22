34a7a5b4bbf3549ff62dd0eb7d4e2bc8c20f3203
Maja Merk <maja.merk@blateral.com>
2022-03-22 17:27:09 +0100

    Adding Index

---
+++ src/components/sections/Index.tsx
@@ -0,0 +1,62 @@
+import Section, { mapToBgMode } from 'components/base/Section';
+import Wrapper from 'components/base/Wrapper';
+import Copy from 'components/typography/Copy';
+import React from 'react';
+import styled from 'styled-components';
+import { useLibTheme } from 'utils/LibThemeProvider';
+import { getColors, spacings } from 'utils/styles';
+
+const List = styled.ul`
+    margin: 0;
+    padding: 0;
+    list-style: none;
+`;
+
+const ListItem = styled.li`
+    padding: ${spacings.nudge}px 0;
+
+    border-top: 1px solid
+        ${({ theme }) => getColors(theme).new.elementBg.medium};
+    border-bottom: 1px solid
+        ${({ theme }) => getColors(theme).new.elementBg.medium};
+`;
+
+const Index: React.FC<{
+    items?: { label?: string; link?: string }[];
+    bgMode?: 'inverted' | 'full';
+}> = ({ items, bgMode }) => {
+    const isInverted = bgMode === 'inverted';
+    const { colors } = useLibTheme();
+    return (
+        <Section
+            addSeperation
+            bgColor={
+                isInverted
+                    ? colors.new.sectionBg.dark
+                    : bgMode
+                    ? colors.new.sectionBg.medium
+                    : colors.new.sectionBg.light
+            }
+            bgMode={mapToBgMode(bgMode)}
+        >
+            <Wrapper>
+                <List>
+                    {items?.map((item, i) => {
+                        return (
+                            <ListItem key={i}>
+                                <Copy
+                                    isInverted={isInverted}
+                                    textColor={colors.new.primary.default}
+                                >
+                                    <a href={item.link}>{item.label}</a>
+                                </Copy>
+                            </ListItem>
+                        );
+                    })}
+                </List>
+            </Wrapper>
+        </Section>
+    );
+};
+
+export default Index;
