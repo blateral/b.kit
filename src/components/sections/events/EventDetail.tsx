a940b339120239616058820bc9b8ccc42ee2de60
Maja Merk <maja.merk@blateral.com>
2022-04-22 12:49:36 +0200

    Adding eventdetail

--- src/components/sections/events/EventDetail.tsx
+++ src/components/sections/events/EventDetail.tsx
@@ -1,18 +1,41 @@
 import Section from 'components/base/Section';
 import Wrapper from 'components/base/Wrapper';
 import { EventProps } from 'components/blocks/EventBlock';
+import Image from 'components/blocks/Image';
 import Tag, { TagProps } from 'components/blocks/Tag';
 import Copy from 'components/typography/Copy';
 import Heading from 'components/typography/Heading';
 import { LinkProps } from 'components/typography/Link';
 import React from 'react';
 import styled from 'styled-components';
-import { spacings } from 'utils/styles';
+import { mq, spacings } from 'utils/styles';
 
 const Content = styled.div`
+    @media ${mq.large} {
+        display: flex;
+        flex-direction: row;
+        align-items: flex-start;
+    }
+`;
+
+const MainContent = styled.div`
+    & > * {
+        margin-bottom: ${spacings.nudge * 5}px;
+    }
+
     & > * + * {
         margin-top: ${spacings.nudge * 3}px;
     }
+
+    @media ${mq.large} {
+        & > * {
+            margin-right: ${spacings.spacer}px;
+        }
+    }
+`;
+
+const StyledImage = styled(Image)`
+    margin-bottom: ${spacings.spacer}px;
 `;
 
 const TagContainer = styled.div`
@@ -29,6 +52,78 @@ const TagWrapper = styled.div`
     padding-left: ${spacings.nudge}px;
 `;
 
+const InfoList = styled.ul`
+    margin: 0;
+    padding: 0;
+    list-style: none;
+
+    @media ${mq.semilarge} {
+        display: flex;
+        flex-direction: row;
+        align-items: flex-start;
+
+        & > * {
+            flex: 0 0 33.33%;
+        }
+    }
+
+    @media ${mq.large} {
+        display: block;
+    }
+`;
+
+const InfoItem = styled.li`
+    & + & {
+        padding-top: ${spacings.nudge * 3}px;
+    }
+
+    @media ${mq.semilarge} {
+        & + & {
+            padding-top: 0;
+            padding-left: ${spacings.spacer}px;
+        }
+    }
+
+    @media ${mq.large} {
+        & + & {
+            padding-left: 0;
+            padding-top: ${spacings.nudge * 5}px;
+        }
+    }
+`;
+
+const InfoTitle = styled(Copy)`
+    margin-bottom: ${spacings.nudge * 3}px;
+`;
+
+const InfoContent = styled.div`
+    display: -ms-grid;
+    display: grid;
+
+    -ms-grid-columns: min-content 1fr;
+    grid-template-columns: min-content 1fr;
+    -ms-grid-rows: min-content 1fr;
+    grid-auto-rows: min-content 1fr;
+
+    & + & {
+        padding-top: ${spacings.nudge * 2}px;
+    }
+
+    & > * + * {
+        padding-left: ${spacings.nudge * 2}px;
+    }
+`;
+
+const Icon = styled.div`
+    height: 20px;
+    width: 20px;
+
+    & > * {
+        height: 20px;
+        width: 20px;
+    }
+`;
+
 const EventDetail: React.FC<{
     anchorId?: string;
 
@@ -37,6 +132,14 @@ const EventDetail: React.FC<{
 
     /** Section background */
     bgMode?: 'inverted' | 'full';
+
+    infoList?: {
+        title?: string;
+        infos?: {
+            icon: (isInverted?: boolean) => React.ReactNode;
+            label?: string;
+        }[];
+    }[];
 
     /** Function to inject custom tag node */
     customTag?: (props: {
@@ -49,7 +152,7 @@ const EventDetail: React.FC<{
     }) => React.ReactNode;
 
     onTagClick?: (tag: TagProps) => void;
-}> = ({ event, bgMode, customTag, onTagClick }) => {
+}> = ({ event, infoList, bgMode, customTag, onTagClick }) => {
     const isInverted = bgMode === 'inverted';
 
     const handleTagClick = (tag: TagProps) =>
@@ -61,57 +164,97 @@ const EventDetail: React.FC<{
             : undefined;
     return (
         <Section>
-            <Wrapper>
+            <Wrapper addWhitespace>
+                {event.image?.small && (
+                    <StyledImage
+                        {...event.image}
+                        coverSpace
+                        isInverted={isInverted}
+                    />
+                )}
                 <Content>
-                    {event.tags && (
-                        <TagContainer>
-                            {event.tags.map(
-                                (tag, i) =>
-                                    tag && (
-                                        <TagWrapper key={'tag_' + i}>
-                                            {customTag ? (
-                                                customTag({
-                                                    key: 'tag_' + i,
-                                                    name: tag.name || '',
-                                                    isInverted: isInverted,
-                                                    isActive: false,
-                                                    link: tag.link || {},
-                                                    clickHandler:
-                                                        handleTagClick(tag),
-                                                })
-                                            ) : (
-                                                <Tag
-                                                    isInverted={isInverted}
-                                                    name={tag.name}
-                                                    link={tag.link}
-                                                    onClick={handleTagClick(
-                                                        tag
-                                                    )}
-                                                />
-                                            )}
-                                        </TagWrapper>
-                                    )
-                            )}
-                        </TagContainer>
-                    )}
-                    {event.title && (
-                        <Heading size="heading-2" isInverted={isInverted}>
-                            {event.title}
-                        </Heading>
-                    )}
-                    {event.infoText && (
-                        <Copy
-                            size="big"
-                            isInverted={isInverted}
-                            innerHTML={event.infoText}
-                        />
-                    )}
-                    {event.text && (
-                        <Copy
-                            size="medium"
-                            isInverted={isInverted}
-                            innerHTML={event.text}
-                        />
+                    <MainContent>
+                        {event.tags && (
+                            <TagContainer>
+                                {event.tags.map(
+                                    (tag, i) =>
+                                        tag && (
+                                            <TagWrapper key={'tag_' + i}>
+                                                {customTag ? (
+                                                    customTag({
+                                                        key: 'tag_' + i,
+                                                        name: tag.name || '',
+                                                        isInverted: isInverted,
+                                                        isActive: false,
+                                                        link: tag.link || {},
+                                                        clickHandler:
+                                                            handleTagClick(tag),
+                                                    })
+                                                ) : (
+                                                    <Tag
+                                                        isInverted={isInverted}
+                                                        name={tag.name}
+                                                        link={tag.link}
+                                                        onClick={handleTagClick(
+                                                            tag
+                                                        )}
+                                                    />
+                                                )}
+                                            </TagWrapper>
+                                        )
+                                )}
+                            </TagContainer>
+                        )}
+                        {event.title && (
+                            <Heading size="heading-2" isInverted={isInverted}>
+                                {event.title}
+                            </Heading>
+                        )}
+                        {event.infoText && (
+                            <Copy
+                                size="big"
+                                isInverted={isInverted}
+                                innerHTML={event.infoText}
+                            />
+                        )}
+                        {event.text && (
+                            <Copy
+                                size="medium"
+                                isInverted={isInverted}
+                                innerHTML={event.text}
+                            />
+                        )}
+                    </MainContent>
+                    {infoList && (
+                        <InfoList>
+                            {infoList.map((items, i) => {
+                                return (
+                                    <InfoItem key={i}>
+                                        <InfoTitle size="small" type="copy-b">
+                                            {items.title}
+                                        </InfoTitle>
+                                        {items.infos &&
+                                            items.infos.map((info, ii) => {
+                                                return (
+                                                    <InfoContent key={ii}>
+                                                        <Icon>
+                                                            {info.icon(
+                                                                isInverted
+                                                            )}
+                                                        </Icon>
+                                                        <Copy
+                                                            size="small"
+                                                            type="copy"
+                                                        >
+                                                            {info.label}
+                                                        </Copy>
+                                                    </InfoContent>
+                                                );
+                                            })}
+                                    </InfoItem>
+                                );
+                            })}
+                        </InfoList>
                     )}
                 </Content>
             </Wrapper>
--- src/stories/sections/events/EventDetail.stories.tsx
+++ src/stories/sections/events/EventDetail.stories.tsx
@@ -1,6 +1,11 @@
+/* eslint-disable react/display-name */
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
 import EventDetail from 'components/sections/events/EventDetail';
+import Magnifier from 'components/base/icons/Magnifier';
+import ExclamationMark from 'components/base/icons/ExclamationMark';
+import Phone from 'components/base/icons/Phone';
+import Mail from 'components/base/icons/Mail';
 
 export default {
     title: 'Sections / Events / EventDetail',
@@ -22,5 +27,128 @@ export const Default: Story = () => (
             ],
             link: { href: '#0' },
         }}
+    />
+);
+
+export const WithImage: Story = () => (
+    <EventDetail
+        event={{
+            image: {
+                small: 'https://unsplash.it/640/274',
+                medium: 'https://unsplash.it/832/357',
+                semilarge: 'https://unsplash.it/1024/439',
+                large: 'https://unsplash.it/832/624',
+                ratios: {
+                    small: { w: 352, h: 176 },
+                    medium: { w: 752, h: 276 },
+                    large: { w: 375, h: 250 },
+                },
+            },
+            title: 'Sitzung des Gemeinderates',
+            infoText: `<p><b>Mo, 01.06.2022 | 18.00 - 23.00 Uhr</b><br />Altes Rathaus | Münsterstraße 23 | 88662 Überlingen</p>`,
+            text: `<p><b>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Blandit turpis cursus in hac habitasse platea dictumst quisque. Elit ut aliquam purus sit amet. Hendrerit dolor magna eget est lorem ipsum dolor sit amet. Sollicitudin tempor id eu nisl nunc mi ipsum.</b><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae ultricies leo integer malesuada. Eget gravida cum sociis natoque penatibus et magnis. Scelerisque viverra mauris in aliquam sem fringilla ut morbi. Eu lobortis elementum nibh tellus molestie nunc. Cursus in hac habitasse platea. Rhoncus aenean vel elit scelerisque mauris pellentesque. Eu scelerisque felis imperdiet proin fermentum leo. In arcu cursus euismod quis viverra nibh cras pulvinar mattis. Fermentum iaculis eu non diam phasellus vestibulum lorem sed risus. Imperdiet dui accumsan sit amet nulla facilisi morbi tempus iaculis.<br7>Sed ullamcorper morbi tincidunt ornare massa eget egestas purus. Ultrices mi tempus imperdiet nulla malesuada pellentesque elit eget gravida. Ut morbi tincidunt augue interdum. Pulvinar neque laoreet suspendisse interdum. Egestas maecenas pharetra convallis posuere morbi leo urna molestie at. Imperdiet sed euismod nisi porta. Iaculis at erat pellentesque adipiscing commodo. Nunc consequat interdum varius sit. Aliquet nibh praesent tristique magna sit amet purus gravida. Faucibus purus in massa tempor nec feugiat nisl pretium fusce. Maecenas accumsan lacus vel facilisis volutpat est. Imperdiet dui accumsan sit amet nulla facilisi morbi tempus iaculis.<br7>Viverra nam libero justo laoreet. In cursus turpis massa tincidunt dui ut ornare. Eu turpis egestas pretium aenean pharetra. Maecenas pharetra convallis posuere morbi leo urna molestie at elementum. Sit amet risus nullam eget felis eget nunc. Et molestie ac feugiat sed lectus vestibulum. Et magnis dis parturient montes nascetur ridiculus mus mauris vitae. Et tortor at risus viverra adipiscing at in tellus integer. Amet aliquam id diam maecenas. In iaculis nunc sed augue. Vitae elementum curabitur vitae nunc sed velit dignissim sodales ut. Nam libero justo laoreet sit amet cursus sit amet dictum. Consectetur purus ut faucibus pulvinar elementum. Nulla malesuada pellentesque elit eget gravida cum sociis. Volutpat sed cras ornare arcu dui vivamus arcu. Aliquam vestibulum morbi blandit cursus risus at. Nascetur ridiculus mus mauris vitae ultricies leo integer malesuada. Tellus rutrum tellus pellentesque eu tincidunt tortor aliquam nulla. Mollis nunc sed id semper risus.</p>`,
+            tags: [
+                { name: 'Tag 1', link: { href: '#0' } },
+                { name: 'Tag 2', link: { href: '#0' } },
+                { name: 'Tag 3', link: { href: '#0' } },
+                { name: 'Tag 4', link: { href: '#0' } },
+                { name: 'Tag 5', link: { href: '#0' } },
+            ],
+            link: { href: '#0' },
+        }}
+    />
+);
+
+export const WithInfoList: Story = () => (
+    <EventDetail
+        event={{
+            image: {
+                small: 'https://unsplash.it/640/274',
+                medium: 'https://unsplash.it/832/357',
+                semilarge: 'https://unsplash.it/1024/439',
+                large: 'https://unsplash.it/832/624',
+                ratios: {
+                    small: { w: 352, h: 176 },
+                    medium: { w: 752, h: 276 },
+                    large: { w: 375, h: 250 },
+                },
+            },
+            title: 'Sitzung des Gemeinderates',
+            infoText: `<p><b>Mo, 01.06.2022 | 18.00 - 23.00 Uhr</b><br />Altes Rathaus | Münsterstraße 23 | 88662 Überlingen</p>`,
+            text: `<p><b>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Blandit turpis cursus in hac habitasse platea dictumst quisque. Elit ut aliquam purus sit amet. Hendrerit dolor magna eget est lorem ipsum dolor sit amet. Sollicitudin tempor id eu nisl nunc mi ipsum.</b><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae ultricies leo integer malesuada. Eget gravida cum sociis natoque penatibus et magnis. Scelerisque viverra mauris in aliquam sem fringilla ut morbi. Eu lobortis elementum nibh tellus molestie nunc. Cursus in hac habitasse platea. Rhoncus aenean vel elit scelerisque mauris pellentesque. Eu scelerisque felis imperdiet proin fermentum leo. In arcu cursus euismod quis viverra nibh cras pulvinar mattis. Fermentum iaculis eu non diam phasellus vestibulum lorem sed risus. Imperdiet dui accumsan sit amet nulla facilisi morbi tempus iaculis.<br7>Sed ullamcorper morbi tincidunt ornare massa eget egestas purus. Ultrices mi tempus imperdiet nulla malesuada pellentesque elit eget gravida. Ut morbi tincidunt augue interdum. Pulvinar neque laoreet suspendisse interdum. Egestas maecenas pharetra convallis posuere morbi leo urna molestie at. Imperdiet sed euismod nisi porta. Iaculis at erat pellentesque adipiscing commodo. Nunc consequat interdum varius sit. Aliquet nibh praesent tristique magna sit amet purus gravida. Faucibus purus in massa tempor nec feugiat nisl pretium fusce. Maecenas accumsan lacus vel facilisis volutpat est. Imperdiet dui accumsan sit amet nulla facilisi morbi tempus iaculis.<br7>Viverra nam libero justo laoreet. In cursus turpis massa tincidunt dui ut ornare. Eu turpis egestas pretium aenean pharetra. Maecenas pharetra convallis posuere morbi leo urna molestie at elementum. Sit amet risus nullam eget felis eget nunc. Et molestie ac feugiat sed lectus vestibulum. Et magnis dis parturient montes nascetur ridiculus mus mauris vitae. Et tortor at risus viverra adipiscing at in tellus integer. Amet aliquam id diam maecenas. In iaculis nunc sed augue. Vitae elementum curabitur vitae nunc sed velit dignissim sodales ut. Nam libero justo laoreet sit amet cursus sit amet dictum. Consectetur purus ut faucibus pulvinar elementum. Nulla malesuada pellentesque elit eget gravida cum sociis. Volutpat sed cras ornare arcu dui vivamus arcu. Aliquam vestibulum morbi blandit cursus risus at. Nascetur ridiculus mus mauris vitae ultricies leo integer malesuada. Tellus rutrum tellus pellentesque eu tincidunt tortor aliquam nulla. Mollis nunc sed id semper risus.</p>`,
+            tags: [
+                { name: 'Tag 1', link: { href: '#0' } },
+                { name: 'Tag 2', link: { href: '#0' } },
+                { name: 'Tag 3', link: { href: '#0' } },
+                { name: 'Tag 4', link: { href: '#0' } },
+                { name: 'Tag 5', link: { href: '#0' } },
+            ],
+            link: { href: '#0' },
+        }}
+        infoList={[
+            {
+                title: 'Allgemeine Infos',
+                infos: [
+                    {
+                        icon: () => <Magnifier />,
+                        label: 'Altes Rauthaus, Münsterstraße 23, 88662 Überlingen',
+                    },
+                    {
+                        icon: () => <Magnifier />,
+                        label: 'Auf Karte anzeigen',
+                    },
+                    {
+                        icon: () => <Magnifier />,
+                        label: 'Eintritt frei',
+                    },
+                    {
+                        icon: () => <ExclamationMark />,
+                        label: 'Lorem ipsum dolor sit amet consectetur adipiscing elit',
+                    },
+                ],
+            },
+            {
+                title: 'Reservierung',
+                infos: [
+                    {
+                        icon: () => <Phone />,
+                        label: '07551 / 888666',
+                    },
+                    {
+                        icon: () => <Mail />,
+                        label: 'max.mustermann@gmail.com',
+                    },
+                    {
+                        icon: () => <Magnifier />,
+                        label: 'www.topevents.com',
+                    },
+                    {
+                        icon: () => <ExclamationMark />,
+                        label: 'Lorem ipsum dolor sit amet consectetur adipiscing elit',
+                    },
+                ],
+            },
+            {
+                title: 'Veranstalter',
+                infos: [
+                    {
+                        icon: () => <Phone />,
+                        label: '07551 / 888666',
+                    },
+                    {
+                        icon: () => <Mail />,
+                        label: 'max.mustermann@gmail.com',
+                    },
+                    {
+                        icon: () => <Magnifier />,
+                        label: 'www.topevents.com',
+                    },
+                    {
+                        icon: () => <ExclamationMark />,
+                        label: 'Lorem ipsum dolor sit amet consectetur adipiscing elit',
+                    },
+                ],
+            },
+        ]}
     />
 );
