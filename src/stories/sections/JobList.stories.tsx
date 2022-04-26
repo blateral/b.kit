cadb915ae7e129dda2f5ffef91f26b9517e1c2cc
Maja Merk <maja.merk@blateral.com>
2022-04-26 15:52:43 +0200

    Adding background to stories

--- src/stories/sections/JobList.stories.tsx
+++ src/stories/sections/JobList.stories.tsx
@@ -58,3 +58,109 @@ export const Default: Story = () => (
         ]}
     />
 );
+
+export const WithBackground: Story = () => (
+    <JobList
+        bgMode="full"
+        jobs={[
+            {
+                jobTitle: 'Jobtitle 1<br/>(m/w/d)',
+                timeModel: 'Vollzeit',
+                location: 'Immenstaad / Ravensburg',
+            },
+            {
+                jobTitle: `Jobtitle 2<br/>(m/w/d)`,
+                timeModel: 'Vollzeit',
+                location: 'Immenstaad / Ravensburg',
+            },
+            {
+                jobTitle: `Jobtitle 3<br/>(m/w/d)`,
+                timeModel: 'Vollzeit',
+                location: 'Immenstaad / Ravensburg',
+            },
+            {
+                jobTitle: `Jobtitle 4<br/>(m/w/d)`,
+                timeModel: 'Vollzeit',
+                location: 'Immenstaad / Ravensburg',
+            },
+            {
+                jobTitle: `Jobtitle 5<br/>(m/w/d)`,
+                timeModel: 'Vollzeit',
+                location: 'Immenstaad / Ravensburg',
+            },
+            {
+                jobTitle: `Jobtitle 6<br/>(m/w/d)`,
+                timeModel: 'Vollzeit',
+                location: 'Immenstaad / Ravensburg',
+            },
+            {
+                jobTitle: `Jobtitle 7<br/>(m/w/d)`,
+                timeModel: 'Vollzeit',
+                location: 'Immenstaad / Ravensburg',
+            },
+            {
+                jobTitle: `Jobtitle 8<br/>(m/w/d)`,
+                timeModel: 'Vollzeit',
+                location: 'Immenstaad / Ravensburg',
+            },
+            {
+                jobTitle: `Jobtitle 9<br/>(m/w/d)`,
+                timeModel: 'Vollzeit',
+                location: 'Immenstaad / Ravensburg',
+            },
+        ]}
+    />
+);
+
+export const IsInverted: Story = () => (
+    <JobList
+        bgMode="inverted"
+        jobs={[
+            {
+                jobTitle: 'Jobtitle 1<br/>(m/w/d)',
+                timeModel: 'Vollzeit',
+                location: 'Immenstaad / Ravensburg',
+            },
+            {
+                jobTitle: `Jobtitle 2<br/>(m/w/d)`,
+                timeModel: 'Vollzeit',
+                location: 'Immenstaad / Ravensburg',
+            },
+            {
+                jobTitle: `Jobtitle 3<br/>(m/w/d)`,
+                timeModel: 'Vollzeit',
+                location: 'Immenstaad / Ravensburg',
+            },
+            {
+                jobTitle: `Jobtitle 4<br/>(m/w/d)`,
+                timeModel: 'Vollzeit',
+                location: 'Immenstaad / Ravensburg',
+            },
+            {
+                jobTitle: `Jobtitle 5<br/>(m/w/d)`,
+                timeModel: 'Vollzeit',
+                location: 'Immenstaad / Ravensburg',
+            },
+            {
+                jobTitle: `Jobtitle 6<br/>(m/w/d)`,
+                timeModel: 'Vollzeit',
+                location: 'Immenstaad / Ravensburg',
+            },
+            {
+                jobTitle: `Jobtitle 7<br/>(m/w/d)`,
+                timeModel: 'Vollzeit',
+                location: 'Immenstaad / Ravensburg',
+            },
+            {
+                jobTitle: `Jobtitle 8<br/>(m/w/d)`,
+                timeModel: 'Vollzeit',
+                location: 'Immenstaad / Ravensburg',
+            },
+            {
+                jobTitle: `Jobtitle 9<br/>(m/w/d)`,
+                timeModel: 'Vollzeit',
+                location: 'Immenstaad / Ravensburg',
+            },
+        ]}
+    />
+);
