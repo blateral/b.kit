bf82353b3474a8f1b6ed8314ef69601881677ec4
Maja Merk <maja.merk@blateral.com>
2022-04-27 17:50:19 +0200

    Adding footer

--- src/components/sections/FooterNew.tsx
+++ src/components/sections/FooterNew.tsx
@@ -1,9 +1,86 @@
+import Grid from 'components/base/Grid';
+import LanguageIcon from 'components/base/icons/Language';
 import Section, { mapToBgMode } from 'components/base/Section';
 import Wrapper from 'components/base/Wrapper';
+import Bdot from 'components/blocks/Bdot';
+import LanguageSwitcher, { Language } from 'components/blocks/LanguageSwitcher';
+import SocialList from 'components/blocks/SocialList';
 import Copy from 'components/typography/Copy';
 import Link from 'components/typography/Link';
 import React from 'react';
+import styled from 'styled-components';
 import { useLibTheme } from 'utils/LibThemeProvider';
+import { getColors as color, mq, spacings } from 'utils/styles';
+
+const MainView = styled.div`
+    & > * {
+        margin-bottom: ${spacings.nudge * 2}px;
+    }
+`;
+
+const ColTitle = styled(Copy)`
+    margin-bottom: ${spacings.nudge * 2}px;
+`;
+
+const LinkList = styled.ul`
+    padding: 0;
+    margin: -${spacings.nudge}px;
+    list-style: none;
+`;
+
+const LinkItem = styled.li`
+    padding: ${spacings.nudge}px;
+`;
+
+const FooterCol = styled.div`
+    & > * + * {
+        margin-top: ${spacings.nudge * 5}px;
+    }
+`;
+
+const Action = styled.div`
+    padding-top: ${spacings.nudge * 2}px;
+`;
+
+const FootNote = styled(Copy)`
+    margin-top: ${spacings.nudge * 5}px;
+`;
+
+const BottomView = styled.div`
+    border-top: 1px solid ${({ theme }) => color(theme).elementBg.medium};
+    padding-top: ${spacings.nudge * 2}px;
+
+    @media ${mq.semilarge} {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+    }
+`;
+
+const LanguageBlock = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    & > * + * {
+        margin-left: ${spacings.nudge / 2}px;
+    }
+`;
+
+const BottomLinkList = styled.ul`
+    padding: 0;
+    margin: -${spacings.nudge}px;
+    list-style: none;
+
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+`;
+
+const BottomLinkItem = styled.li`
+    padding: ${spacings.nudge}px;
+`;
 
 const FooterNew: React.FC<{
     bgMode?: 'inverted';
@@ -11,8 +88,36 @@ const FooterNew: React.FC<{
         siteColTitle?: string;
         colLinks?: { href?: string; label?: string }[];
     }[];
-}> = ({ bgMode, siteLinks }) => {
+    callToAction?: {
+        title?: string;
+        text?: string;
+        action?: (isInverted?: boolean) => React.ReactNode;
+    };
+
+    socials?: {
+        title?: string;
+        social: {
+            icon?: (isInverted?: boolean) => React.ReactNode;
+            href?: string;
+        }[];
+    };
+    footNote?: string;
+    bottomLinks?: { href: string; label?: string; isExternal?: boolean }[];
+    language?: Language[];
+    languageIcon?: boolean;
+}> = ({
+    bgMode,
+    siteLinks,
+    callToAction,
+    socials,
+    footNote,
+    bottomLinks,
+    languageIcon,
+    language,
+}) => {
     const { colors } = useLibTheme();
+
+    const isInverted = bgMode === 'inverted';
     return (
         <Section
             renderAs="footer"
@@ -20,25 +125,143 @@ const FooterNew: React.FC<{
             bgColor={bgMode ? colors.sectionBg.dark : colors.sectionBg.light}
         >
             <Wrapper addWhitespace>
-                {siteLinks &&
-                    siteLinks.map((links, i) => {
-                        return (
-                            <div key={i} className="col">
-                                <Copy type="copy-b">{links.siteColTitle}</Copy>
-                                <ul>
-                                    {links.colLinks?.map((link, ii) => {
-                                        return (
-                                            <li key={ii}>
-                                                <Link href={link.href}>
-                                                    <Copy>{link.label}</Copy>
-                                                </Link>
-                                            </li>
-                                        );
-                                    })}
-                                </ul>
-                            </div>
-                        );
-                    })}
+                <MainView>
+                    <Grid.Row>
+                        {siteLinks &&
+                            siteLinks.map((links, i) => {
+                                return (
+                                    <Grid.Col
+                                        semilarge={{ span: 4 / 12 }}
+                                        large={{ span: 3 / 12 }}
+                                        key={i}
+                                        className="col"
+                                    >
+                                        <ColTitle
+                                            type="copy-b"
+                                            isInverted={isInverted}
+                                        >
+                                            {links.siteColTitle}
+                                        </ColTitle>
+                                        <LinkList>
+                                            {links.colLinks?.map((link, ii) => {
+                                                return (
+                                                    <LinkItem key={ii}>
+                                                        <Link
+                                                            isInverted={
+                                                                isInverted
+                                                            }
+                                                            href={link.href}
+                                                        >
+                                                            <Copy
+                                                                isInverted={
+                                                                    isInverted
+                                                                }
+                                                            >
+                                                                {link.label}
+                                                            </Copy>
+                                                        </Link>
+                                                    </LinkItem>
+                                                );
+                                            })}
+                                        </LinkList>
+                                    </Grid.Col>
+                                );
+                            })}
+                        <Grid.Col large={{ span: 3 / 12 }}>
+                            <FooterCol>
+                                {callToAction && (
+                                    <div>
+                                        <ColTitle
+                                            type="copy-b"
+                                            isInverted={isInverted}
+                                        >
+                                            {callToAction.title}
+                                        </ColTitle>
+                                        <Copy isInverted={isInverted}>
+                                            {callToAction.text}
+                                        </Copy>
+
+                                        {callToAction.action && (
+                                            <Action>
+                                                {callToAction.action(
+                                                    isInverted
+                                                )}
+                                            </Action>
+                                        )}
+                                    </div>
+                                )}
+                                {socials && (
+                                    <div>
+                                        <ColTitle
+                                            isInverted={isInverted}
+                                            type="copy-b"
+                                        >
+                                            {socials.title}
+                                        </ColTitle>
+                                        <SocialList
+                                            isInverted={isInverted}
+                                            items={socials.social.map(
+                                                (item) => {
+                                                    return {
+                                                        href: item.href,
+                                                        icon: item.icon,
+                                                    };
+                                                }
+                                            )}
+                                        />
+                                    </div>
+                                )}
+                            </FooterCol>
+                        </Grid.Col>
+                    </Grid.Row>
+                    {footNote && (
+                        <FootNote
+                            isInverted={isInverted}
+                            innerHTML={footNote}
+                        />
+                    )}
+                </MainView>
+                <BottomView>
+                    <LanguageBlock>
+                        {languageIcon && (
+                            <span>
+                                <LanguageIcon
+                                    iconColor={
+                                        isInverted
+                                            ? colors.text.inverted
+                                            : colors.text.default
+                                    }
+                                />
+                            </span>
+                        )}
+                        {language && (
+                            <LanguageSwitcher
+                                isInverted={isInverted}
+                                langs={language}
+                            />
+                        )}
+                    </LanguageBlock>
+                    <BottomLinkList>
+                        {bottomLinks?.map((bottomLink, i) => {
+                            return (
+                                <BottomLinkItem key={i}>
+                                    <Link
+                                        isInverted={isInverted}
+                                        href={bottomLink.href}
+                                        isExternal={bottomLink.isExternal}
+                                    >
+                                        <Copy isInverted={isInverted}>
+                                            {bottomLink.label}
+                                        </Copy>
+                                    </Link>
+                                </BottomLinkItem>
+                            );
+                        })}
+                        <BottomLinkItem>
+                            <Bdot />
+                        </BottomLinkItem>
+                    </BottomLinkList>
+                </BottomView>
             </Wrapper>
         </Section>
     );
--- src/stories/sections/FooterNew.stories.tsx
+++ src/stories/sections/FooterNew.stories.tsx
@@ -1,6 +1,12 @@
+/* eslint-disable react/display-name */
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
 import FooterNew from 'components/sections/FooterNew';
+import Facebook from 'components/base/icons/socials/Facebook';
+import Instagram from 'components/base/icons/socials/Instagram';
+import Youtube from 'components/base/icons/socials/Youtube';
+import ButtonGhost from 'components/buttons/ButtonGhost';
+import Copy from 'components/typography/Copy';
 
 const exampleSiteLinks = [
     {
@@ -80,6 +86,31 @@ const exampleSiteLinks = [
     },
 ];
 
+const exampleCallToAction = {
+    title: 'Newsletter',
+    text: 'Immer gut informiert. Abbonieren Sie unseren Newsletter um regelmäßig Informationen zu bekommen.',
+    action: (isInverted?: boolean) => (
+        <ButtonGhost.View isInverted={isInverted}>
+            <ButtonGhost.Label>Primary</ButtonGhost.Label>
+        </ButtonGhost.View>
+    ),
+};
+
+const exampleSocials = {
+    title: 'Folgen Sie Uns',
+    social: [
+        { href: '#', icon: () => <Facebook /> },
+        { href: '#', icon: () => <Instagram /> },
+        { href: '#', icon: () => <Youtube /> },
+    ],
+};
+
+const exampleBottomLinks = [
+    { href: '/impressum', label: 'Impressum' },
+    { href: '/agb', label: 'AGB' },
+    { href: '/datenschutz', label: 'Datenschutz' },
+];
+
 export default {
     title: 'Sections / FooterNew',
     component: FooterNew,
@@ -87,6 +118,77 @@ export default {
 
 export const Default: Story = () => <FooterNew siteLinks={exampleSiteLinks} />;
 
+export const WithCallToAction: Story = () => (
+    <FooterNew
+        siteLinks={exampleSiteLinks}
+        callToAction={exampleCallToAction}
+    />
+);
+
+export const WithSocials: Story = () => (
+    <FooterNew
+        siteLinks={exampleSiteLinks}
+        callToAction={exampleCallToAction}
+        socials={exampleSocials}
+    />
+);
+
+export const WithFootNote: Story = () => (
+    <FooterNew
+        siteLinks={exampleSiteLinks}
+        callToAction={exampleCallToAction}
+        socials={exampleSocials}
+        footNote={`<b>© Villa Wunschgarten</b> | Münsterstraße 157 | 88662 Überlingen | Tel: 07551 8112345 | info@villa-wunschgarten.de`}
+    />
+);
+
+export const WithBottomLinks: Story = () => (
+    <FooterNew
+        siteLinks={exampleSiteLinks}
+        callToAction={exampleCallToAction}
+        socials={exampleSocials}
+        footNote={`<b>© Villa Wunschgarten</b> | Münsterstraße 157 | 88662 Überlingen | Tel: 07551 8112345 | info@villa-wunschgarten.de`}
+        bottomLinks={exampleBottomLinks}
+    />
+);
+
+export const WithLanguageSwitcher: Story = () => (
+    <FooterNew
+        siteLinks={exampleSiteLinks}
+        callToAction={exampleCallToAction}
+        socials={exampleSocials}
+        footNote={`<b>© Villa Wunschgarten</b> | Münsterstraße 157 | 88662 Überlingen | Tel: 07551 8112345 | info@villa-wunschgarten.de`}
+        bottomLinks={exampleBottomLinks}
+        language={[
+            { label: 'deutsch', link: { href: '/de' } },
+            { label: 'englisch', link: { href: '/en' } },
+            { label: 'französisch', link: { href: '/fr' } },
+        ]}
+    />
+);
+
+export const WithLanguageSwitcherIcon: Story = () => (
+    <FooterNew
+        siteLinks={exampleSiteLinks}
+        callToAction={exampleCallToAction}
+        socials={exampleSocials}
+        footNote={`<b>© Villa Wunschgarten</b> | Münsterstraße 157 | 88662 Überlingen | Tel: 07551 8112345 | info@villa-wunschgarten.de`}
+        bottomLinks={exampleBottomLinks}
+        language={[
+            { label: 'deutsch', link: { href: '/de' } },
+            { label: 'englisch', link: { href: '/en' } },
+            { label: 'französisch', link: { href: '/fr' } },
+        ]}
+        languageIcon
+    />
+);
+
 export const IsInverted: Story = () => (
-    <FooterNew bgMode="inverted" siteLinks={exampleSiteLinks} />
+    <FooterNew
+        bgMode="inverted"
+        siteLinks={exampleSiteLinks}
+        callToAction={exampleCallToAction}
+        socials={exampleSocials}
+        footNote={`<b>© Villa Wunschgarten</b> | Münsterstraße 157 | 88662 Überlingen | Tel: 07551 8112345 | info@villa-wunschgarten.de`}
+    />
 );
