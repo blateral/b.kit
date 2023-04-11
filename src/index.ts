2dd51a2e085e7974a2d970b31643d91c0b566dc2
Lukas Wochner <lukas.wochner@blateral.com>
2023-04-11 20:19:57 +0200

    adding code splitting setup (WIP)

--- src/index.ts
+++ src/index.ts
@@ -24,55 +24,6 @@ export {
     mq,
 } from 'utils/styles';
 
-/***** Hooks *****/
-export { useEqualSheetHeight } from 'utils/useEqualSheetHeight';
-export {
-    setCookie,
-    deleteCookie,
-    getCookie,
-} from 'utils/cookie-consent/cookie';
-export { default as useIE } from 'utils/useIE';
-export { useItemHeight } from 'utils/useItemHeight';
-export { useFontsLoaded } from 'utils/useFontsLoaded';
-export {
-    getMediaQuery,
-    isBrowser,
-    useMediaQuery,
-    useMediaQueries,
-} from 'utils/useMediaQuery';
-export { useScroll, ScrollDirection } from 'utils/useScroll';
-export { useScrollTo } from 'utils/useScrollTo';
-export { usePoster, canUseWebP } from 'utils/usePoster';
-export { default as useInterval } from 'utils/useInterval';
-export { default as useTouch } from 'utils/useTouch';
-export { default as useUpdateEffect } from 'utils/useUpdateEffect';
-export { default as useMounted } from 'utils/useMounted';
-export { default as useTimeout } from 'utils/useTimeout';
-export { default as useDebounce } from 'utils/useDebounce';
-export { default as useScrollThrottle } from 'utils/useScrollThrottle';
-export { default as useResizeThrottle } from 'utils/useResizeThrottle';
-export { default as useLazyInput } from 'utils/useLazyInput';
-export { default as useArray } from 'utils/useArray';
-export { useImgPreload } from 'utils/useImgPreload';
-export { hexToRgba } from 'utils/hexRgbConverter';
-export { getBgImage } from 'utils/backgroundImage';
-export { clampValue } from 'utils/clamp';
-export { importFonts } from 'utils/fontLoading';
-export { getCurrentNavItem, getCurrentNavPath } from 'utils/navigation';
-export { isEmptyArray, isValidArray } from 'utils/arrays';
-export { concat, printAnchorTag } from 'utils/concat';
-export { getLinkIcon } from 'utils/getLinkIcon';
-export { default as useLeafletMap } from 'utils/useLeafletMap';
-export { default as useParams } from 'utils/useParams';
-export { default as useGeolocation } from 'utils/useGeolocation';
-export { useObserverSupport } from 'utils/useObserverSupport';
-export * from 'utils/urlParams';
-export * from 'utils/dataURI';
-export {
-    default as useCookieConsent,
-    selectors,
-} from 'utils/cookie-consent/useCookieConsent';
-
 /***** Component exports *****/
 export * from 'utils/LibThemeProvider';
 
@@ -140,6 +91,7 @@ export {
     gridSettings,
     getGridWidth,
 } from 'components/base/Grid';
+
 export { default as Section, mapToBgMode } from 'components/base/Section';
 export { default as Wrapper, wrapperWhitespace } from 'components/base/Wrapper';
 
@@ -182,28 +134,4 @@ export {
     CookieTypeSelect,
 } from 'components/blocks/CookieConsent';
 
-/***** Navigation *****/
-export {
-    default as Navigation,
-    getFullNavbarHeights,
-    getNavbarState,
-    generateNavbarIdent,
-} from 'components/sections/navigation/Navigation';
-export * as NavPartials from 'components/sections/navigation/partials/index';
-export * as NavSkeletons from 'components/sections/navigation/skeletons/index';
-
-export { default as NavBarMainPartial } from 'components/sections/navigation/partials/NavBarMain';
-export { default as NavBarTopPartial } from 'components/sections/navigation/partials/NavBarTop';
-export { default as NavBarBottomPartial } from 'components/sections/navigation/partials/NavBarBottom';
-export { default as MenuHeaderPartial } from 'components/sections/navigation/partials/MenuHeader';
-export { default as MenuFooterPartial } from 'components/sections/navigation/partials/MenuFooter';
-
 export { default as SocialList } from 'components/blocks/SocialList';
-
-/***** Footer *****/
-export { default as Footer } from 'components/sections/footer/Footer';
-export * as FooterPartials from 'components/sections/footer/partials/index';
-export * as FooterSkeletons from 'components/sections/footer/skeletons/index';
-
-/***** Icons *****/
-export * as Icons from 'components/base/icons/Icons';
---
+++ src/navigation.ts
@@ -0,0 +1,10 @@
+/***** Navigation *****/
+export * from 'components/sections/navigation/Navigation';
+export * as NavPartials from 'components/sections/navigation/partials/index';
+export * as NavSkeletons from 'components/sections/navigation/skeletons/index';
+
+export * from 'components/sections/navigation/partials/NavBarMain';
+export * from 'components/sections/navigation/partials/NavBarTop';
+export * from 'components/sections/navigation/partials/NavBarBottom';
+export * from 'components/sections/navigation/partials/MenuHeader';
+export * from 'components/sections/navigation/partials/MenuFooter';
