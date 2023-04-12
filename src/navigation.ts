407b184da416130c8efd0fa00d503249e5b595bd
Lukas Wochner <lukas.wochner@blateral.com>
2023-04-12 12:32:12 +0200

    fixing navigation entrypoint

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,10 +1,15 @@
 /***** Navigation *****/
-export { default as Navigation } from 'components/sections/navigation/Navigation';
+export {
+    default as Navigation,
+    getFullNavbarHeights,
+    getNavbarState,
+    generateNavbarIdent,
+} from 'components/sections/navigation/Navigation';
 export * as NavPartials from 'components/sections/navigation/partials/index';
 export * as NavSkeletons from 'components/sections/navigation/skeletons/index';
 
-export * from 'components/sections/navigation/partials/NavBarMain';
-export * from 'components/sections/navigation/partials/NavBarTop';
-export * from 'components/sections/navigation/partials/NavBarBottom';
-export * from 'components/sections/navigation/partials/MenuHeader';
-export * from 'components/sections/navigation/partials/MenuFooter';
+export { default as NavBarMainPartial } from 'components/sections/navigation/partials/NavBarMain';
+export { default as NavBarTopPartial } from 'components/sections/navigation/partials/NavBarTop';
+export { default as NavBarBottomPartial } from 'components/sections/navigation/partials/NavBarBottom';
+export { default as MenuHeaderPartial } from 'components/sections/navigation/partials/MenuHeader';
+export { default as MenuFooterPartial } from 'components/sections/navigation/partials/MenuFooter';
