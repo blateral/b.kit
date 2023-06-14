ed82768af6fff0d3d727d9f86c5caf405ad1c7af
Maja Merk <maja.merk@blateral.com>
2023-06-14 09:54:18 +0200

    Adding bg and anchor id to code component

--- src/components/sections/HtmlCode.tsx
+++ src/components/sections/HtmlCode.tsx
@@ -1,11 +1,28 @@
-import Section from 'components/base/Section';
+import Section, { mapToBgMode } from 'components/base/Section';
 import Wrapper from 'components/base/Wrapper';
 import React from 'react';
-import { withLibTheme } from 'utils/LibThemeProvider';
+import { useLibTheme, withLibTheme } from 'utils/LibThemeProvider';
 
-const HtmlCode: React.FC<{ html: string }> = ({ html }) => {
+const HtmlCode: React.FC<{
+    html: string;
+    anchorId?: string;
+    bgMode?: 'full' | 'inverted';
+}> = ({ html, anchorId, bgMode }) => {
+    const isInverted = bgMode === 'inverted';
+    const { colors } = useLibTheme();
     return (
-        <Section>
+        <Section
+            addSeperation
+            anchorId={anchorId}
+            bgColor={
+                isInverted
+                    ? colors.sectionBg.dark
+                    : bgMode
+                    ? colors.sectionBg.medium
+                    : colors.sectionBg.light
+            }
+            bgMode={mapToBgMode(bgMode, true)}
+        >
             <Wrapper>
                 <div dangerouslySetInnerHTML={{ __html: html }} />
             </Wrapper>
