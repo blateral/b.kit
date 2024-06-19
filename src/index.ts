6f35d191f5ab11836534caa91d3aad9cdef84e5a
MrDigii <lukas.wochner@blateral.com>
2024-06-19 14:59:45 +0200

    adding form fields to export

--- src/index.ts
+++ src/index.ts
@@ -107,6 +107,13 @@ export { default as Link } from 'components/typography/Link';
 /***** Fields *****/
 export { default as CompactForm } from 'components/fields/CompactForm';
 export { default as SearchInput } from 'components/fields/SearchInput';
+export { default as SelectDropdown } from 'components/fields/SelectDropdown';
+export { default as Textarea } from 'components/fields/Textarea';
+export { default as Textfield } from 'components/fields/Textfield';
+export { default as Radio } from 'components/fields/Radio';
+export { default as Checkbox } from 'components/fields/Checkbox';
+export { default as FileUpload } from 'components/fields/FileUpload';
+export { default as Datepicker } from 'components/fields/Datepicker';
 
 /***** Blocks *****/
 export { default as Title } from 'components/blocks/Title';
